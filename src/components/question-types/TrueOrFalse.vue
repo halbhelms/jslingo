66e4194ea664d50d959b34082114fef095866f82
Hal Helms <hal.helms@gmail.com>
2021-07-22 20:41:29 -0700

    570 questions completed

--- src/components/question-types/TrueOrFalse.vue
+++ src/components/question-types/TrueOrFalse.vue
@@ -46,10 +46,10 @@ export default {
       showMoreInfo: false,
       
       question: {
-      "id": 4340,
+      "id": 4342,
       "type": "TrueOrFalse",
-      "given": "class Machine {<br>&nbsp;&nbsp;constructor(type, manufacturer) {<br>&nbsp;&nbsp;&nbsp;&nbsp;this.type = type<br>&nbsp;&nbsp;&nbsp;&nbsp;this.manufacturer = manufacturer<br>&nbsp;&nbsp;}<br>&nbsp;&nbsp;render() {<br>&nbsp;&nbsp;&nbsp;&nbsp;return 'I am a ' + this.type + ' made by ' + this.manufacturer<br>&nbsp;&nbsp;}<br>}<br><br>class PrintingPress extends Machine{<br>&nbsp;&nbsp;constructor(manufacturer, yearMade) {<br>&nbsp;&nbsp;&nbsp;&nbsp;super('printing press', manufacturer)<br>&nbsp;&nbsp;&nbsp;&nbsp;this.yearMade = yearMade<br>&nbsp;&nbsp;}<br><br>&nbsp;&nbsp;render() {<br>&nbsp;&nbsp;&nbsp;&nbsp;return super.render() + ' I was made in ' + this.yearMade<br>&nbsp;&nbsp;}<br>}<br><br>let printingPress = new PrintingPress('Heidelberg', 2021)<br><br>log(printingPress.render())",
-      "question": "This code will break as <pre>super.render()</pre> is not valid",
+      "given": "class Machine {<br>&nbsp;&nbsp;constructor(type, manufacturer) {<br>&nbsp;&nbsp;&nbsp;&nbsp;this.type = type<br>&nbsp;&nbsp;&nbsp;&nbsp;this.manufacturer = manufacturer<br>&nbsp;&nbsp;}<br>&nbsp;&nbsp;render() {<br>&nbsp;&nbsp;&nbsp;&nbsp;return 'I am a ' + this.type + ' made by ' + this.manufacturer<br>&nbsp;&nbsp;}<br>}<br><br>class PrintingPress extends Machine{<br>&nbsp;&nbsp;constructor(type, manufacturer, yearMade) {<br>&nbsp;&nbsp;&nbsp;&nbsp;this.yearMade = yearMade<br>&nbsp;&nbsp;&nbsp;&nbsp;super(type, manufacturer)<br>&nbsp;&nbsp;}<br>}<br><br>let printingPress = new PrintingPress('Printing Press', 'Heidelberg')<br><br>log(printingPress.render())",
+      "question": "This logs <em>I am a Printing Press made by Heidelberg</em> to the console",
       "displayAsCode": ["given"],
       "q1": "",
       "q2": "",
@@ -57,7 +57,7 @@ export default {
       "answer": false,
       "answers": [],
       "explanation": "",
-      "moreInfo": "https://javascript.info/class-inheritance#overriding-a-method",
+      "moreInfo": "https://javascript.info/class-inheritance#overriding-constructor",
       "size": 0
     },
     }
